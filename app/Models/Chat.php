fb589531c0384ad2ff874e98ad3c517d29b7dbbb
sald19 <carl.sald19@gmail.com>
2022-01-11 11:47:02 -0600

    add relations in models

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Chat extends Model
 {
     use HasFactory;
+
+    public function messages(): HasMany 
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
