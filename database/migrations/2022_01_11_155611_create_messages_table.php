23d3fed5ff4a820b715e74807060c9b429ecace0
sald19 <carl.sald19@gmail.com>
2022-01-11 11:48:43 -0600

    add relations in migration

--- database/migrations/2022_01_11_155611_create_messages_table.php
+++ database/migrations/2022_01_11_155611_create_messages_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Chat;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,6 +18,9 @@ class CreateMessagesTable extends Migration
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
             $table->text('content');
+
+            $table->foreignIdFor(Chat::class);
+            $table->foreignIdFor(User::class);
 
             $table->timestamps();
             $table->softDeletes();
