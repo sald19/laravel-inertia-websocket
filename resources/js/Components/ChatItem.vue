64f549fcc8cdceb4d0279aa8c535cf9803ccfbf6
sald19 <carl.sald19@gmail.com>
2022-01-11 16:35:53 -0600

    add icon inline for avatar

--- resources/js/Components/ChatItem.vue
+++ resources/js/Components/ChatItem.vue
@@ -4,21 +4,30 @@
             <div
                 class="flex flex-col w-10 h-10 justify-center items-center mr-4"
             >
-                <a href="#" class="block relative">
+                <div class="relative inline-block">
                     <img
-                        class="inline-block h-10 w-10 rounded-full ring-2 ring-white"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
+                        class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"
+                        src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt="Profile image"
                     />
-                </a>
+                    <span
+                        class="absolute bottom-0 right-0 inline-block w-3 h-3 bg-green-600 border-2 border-white rounded-full"
+                    ></span>
+                </div>
             </div>
-            <div class="flex-1 pl-1 mr-16">
-                <div class="font-medium">{{ member.name }}</div>
+            <div class="flex-1 pl-1">
+                <div class="flex">
+                    <div class="flex flex-1 text-sm font-semibold">
+                        {{ member.name }}
+                    </div>
+                    <div class="text-gray-600 dark:text-gray-800 text-xs">
+                        6:00 AM
+                    </div>
+                </div>
                 <div class="text-gray-600 dark:text-gray-800 text-sm">
                     Developer
                 </div>
             </div>
-            <div class="text-gray-600 dark:text-gray-800 text-xs">6:00 AM</div>
         </div>
     </li>
 </template>
